71d1a273af060ef0bd7f4559a0ec2a1262b21050
godwinksam <godwinksam@gmail.com>
2026-03-26 23:41:56 +0530

    config file changes

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', "http://" . $_SERVER['HTTP_HOST']),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
