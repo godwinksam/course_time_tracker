71f7296e822f885f6a623d988feba951766e562a
godwinksam <godwinksam@gmail.com>
2026-03-27 23:01:53 +0530

    Fix url path in index.js

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory('/course_hour_tracker/'),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes
 });
 
